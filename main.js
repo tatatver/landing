ccb26f147b06c49a997994b1a2dc84b7fc4a5dfe
tatatver <tata.tver2001@gmail.com>
2024-12-21 00:50:25 +0500

    add second timer

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
-function correctDate(date){
-    return date < 10 ? "0" + date : date;
+function correctDate(date) {
+  return date < 10 ? "0" + date : date;
 }
 
-function saleDate(){
-    const day = document.getElementsByClassName("time")[0];
-    const hours = document.getElementsByClassName("time")[1];
-    const minuts = document.getElementsByClassName("time")[2];
-    const seconds = document.getElementsByClassName("time")[3];
-    const finishDate = new Date(2024,11,31);
+function saleDate(containerId) {
+    const container = document.getElementById(containerId);
+    const times = container.getElementsByClassName("time");
+  
+    const finishDate = new Date(2024, 11, 31);
     const currentDate = new Date();
     const date = finishDate - currentDate;
-    day.innerHTML = correctDate(Math.trunc(date/(1000*60*60*24)));
-    hours.innerHTML=correctDate(Math.trunc((date%(1000*60*60*24)/(1000*60*60))));
-    minuts.innerHTML=correctDate(Math.trunc((date % (1000*60*60))/(1000*60)));
-    seconds.innerHTML=correctDate(Math.trunc((date%(1000*60))/1000));
-}
+  
+      times[0].innerHTML = correctDate(Math.trunc(date / (1000 * 60 * 60 * 24)));
+      times[1].innerHTML = correctDate(Math.trunc((date % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+      times[2].innerHTML = correctDate(Math.trunc((date % (1000 * 60 * 60)) / (1000 * 60)));
+      times[3].innerHTML = correctDate(Math.trunc((date % (1000 * 60)) / 1000));
 
-saleDate() /*вызов функции*/
-setInterval(function(){  /*вызов функции каждую секунду*/
-    saleDate();
-}, 1000)
+  }
 
 
+setInterval(function () {  /*вызов функции каждую секунду*/ 
+  saleDate("timerFirst");
+  saleDate("timerSecond");
+}, 1000);
 
-
+startTimes();
